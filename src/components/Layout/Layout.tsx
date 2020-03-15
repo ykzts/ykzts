126a3879f360f42285359d3320aceefe17f505c9
Yamagishi Kazutoshi <ykzts@desire.sh>
2020-03-15 13:55:14 +0900

    feat: add patreon link (#179)

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,16 +1,13 @@
 import clsx from 'clsx'
 import Head from 'next/head'
 import React, { FC } from 'react'
-import { FaFacebookF, FaGithub, FaTwitter } from 'react-icons/fa'
 import { useInView } from 'react-intersection-observer'
 import css from 'styled-jsx/css'
 import paradigm from '../../assets/paradigm.svg'
-import ExternalLink from '../ExternalLink'
+import Footer from '../Footer'
 import Image from '../Image'
-import List, { ListItem, ListTitle } from '../List'
 import Section, {
   SectionContent,
-  SectionFooter,
   SectionHeader,
   SectionTagline,
   SectionTitle
@@ -23,32 +20,6 @@ const { className: fillClassName, styles: fillStyles } = css.resolve`
     position: absolute;
     top: 0;
     width: 100%;
-  }
-`
-
-const { className: iconClassName, styles: iconStyles } = css.resolve`
-  a {
-    border-bottom: 0;
-    border-radius: 2.25rem;
-    display: inline-block;
-    font-size: 1.25rem;
-    height: 2.25rem;
-    line-height: 2.25rem;
-    position: relative;
-    text-align: center;
-    text-decoration: none;
-    transition: background-color 0.25s ease-in-out;
-    width: 2.25rem;
-  }
-
-  a:hover {
-    background-color: rgba(144, 144, 144, 0.1);
-  }
-`
-
-const { className: footerClassName, styles: footerStyles } = css.resolve`
-  a {
-    color: inherit;
   }
 `
 
@@ -90,66 +61,7 @@ const Layout: FC = ({ children }) => {
 
         <main>{children}</main>
 
-        <Section last>
-          <SectionHeader>
-            <SectionTitle>Get in touch</SectionTitle>
-          </SectionHeader>
-
-          <SectionContent></SectionContent>
-
-          <SectionFooter>
-            <List>
-              <ListItem>
-                <ListTitle>Email</ListTitle>
-
-                <a href="mailto:ykzts@desire.sh">ykzts@desire.sh</a>
-              </ListItem>
-              <ListItem>
-                <ListTitle>Elsewhere</ListTitle>
-
-                <ul className="icons">
-                  <li className="icons__item">
-                    <ExternalLink
-                      className={iconClassName}
-                      href="https://twitter.com/ykzts"
-                    >
-                      <FaTwitter />
-                    </ExternalLink>
-                  </li>
-                  <li className="icons__item">
-                    <ExternalLink
-                      className={iconClassName}
-                      href="https://www.facebook.com/ykzts"
-                    >
-                      <FaFacebookF />
-                    </ExternalLink>
-                  </li>
-                  <li className="icons__item">
-                    <ExternalLink
-                      className={iconClassName}
-                      href="https://github.com/ykzts"
-                    >
-                      <FaGithub />
-                    </ExternalLink>
-                  </li>
-                </ul>
-              </ListItem>
-            </List>
-          </SectionFooter>
-        </Section>
-
-        <footer className="footer">
-          <div className="copyright">
-            © Yamagishi Kazutosh. Design{' '}
-            <ExternalLink
-              className={footerClassName}
-              href="https://html5up.net/"
-            >
-              HTML5 UP
-            </ExternalLink>
-            .
-          </div>
-        </footer>
+        <Footer />
       </div>
 
       <style jsx>{`
@@ -199,60 +111,9 @@ const Layout: FC = ({ children }) => {
         .wrapper--active::before {
           opacity: 1;
         }
-
-        .icons {
-          cursor: default;
-          list-style: none;
-          padding-left: 0;
-        }
-
-        .icons__item {
-          display: inline-block;
-        }
-
-        .icons__item:last-child {
-          padding-right: 0;
-        }
-
-        .footer {
-          color: rgba(0, 0, 0, 0.25);
-          font-size: 1rem;
-          left: 50vw;
-          padding: 0 5rem;
-          position: relative;
-          width: 50vw;
-        }
-
-        @media (max-width: 1280px) {
-          .footer {
-            padding: 0 4rem;
-          }
-        }
-
-        @media (max-width: 1152px) {
-          .footer {
-            left: 0;
-            padding: 0 4rem 4rem 4rem;
-            width: 100%;
-          }
-        }
-
-        @media (max-width: 736px) {
-          .footer {
-            padding: 0 2rem 2rem 2rem;
-          }
-        }
-
-        @media (max-width: 360px) {
-          .footer {
-            padding: 0 1.5rem 1.5rem 1.5rem;
-          }
-        }
       `}</style>
 
       {fillStyles}
-      {iconStyles}
-      {footerStyles}
     </>
   )
 }
