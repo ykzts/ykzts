cd6e6b66043f529006a261c6269321319b637fc4
Yamagishi Kazutoshi <ykzts@desire.sh>
2023-09-05 17:34:29 +0000

    fix: set `rel="noopener"` to external links

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,5 +1,26 @@
 import { type MDXComponents } from 'mdx/types'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return components
+  return {
+    ...components,
+    a({ href, rel, target, ...props }) {
+      const isExternal =
+        !!href && (href.startsWith('https://') || href.startsWith('http://'))
+      const relList = new Set(rel ? rel.split(/\s+/) : [])
+
+      if (isExternal) {
+        relList.add('noreferrer')
+        relList.add('noopener')
+      }
+
+      return (
+        <a
+          href={href}
+          rel={[...relList].join(' ')}
+          target={target ?? isExternal ? '_blank' : undefined}
+          {...props}
+        />
+      )
+    }
+  }
 }
