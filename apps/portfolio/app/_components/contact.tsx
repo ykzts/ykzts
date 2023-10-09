7ad6b56900a2118d8be3fe63a8757cf6c4b58920
Yamagishi Kazutoshi <ykzts@desire.sh>
2023-10-09 07:17:01 +0000

    feat(web): simplify not found

--- apps/portfolio/app/_components/contact.tsx
+++ apps/portfolio/app/_components/contact.tsx
@@ -1,3 +1,4 @@
+import { type ReactNode } from 'react'
 import Link from '@/components/link'
 import styles from './contact.module.css'
 import Section, {
@@ -6,6 +7,60 @@ import Section, {
   SectionHeader,
   SectionTitle
 } from './section'
+
+type SocialLink = {
+  label: string
+  logo: ReactNode
+  url: `https://${string}`
+}
+
+const socialLinks: SocialLink[] = [
+  {
+    label: '山岸和利のFacebookアカウント',
+    logo: (
+      <svg className={styles['icon-logo']}>
+        <use xlinkHref="#facebook-logo" />
+      </svg>
+    ),
+    url: 'https://www.facebook.com/ykzts'
+  },
+  {
+    label: '山岸和利のGitHubアカウント',
+    logo: (
+      <svg className={styles['icon-logo']}>
+        <use xlinkHref="#github-logo" />
+      </svg>
+    ),
+    url: 'https://github.com/ykzts'
+  },
+  {
+    label: '山岸和利のMastodonアカウント',
+    logo: (
+      <svg className={styles['icon-logo']}>
+        <use xlinkHref="#mastodon-logo" />
+      </svg>
+    ),
+    url: 'https://ykzts.technology/@ykzts'
+  },
+  {
+    label: '山岸和利のThreadsアカウント',
+    logo: (
+      <svg className={styles['icon-logo']}>
+        <use xlinkHref="#threads-logo" />
+      </svg>
+    ),
+    url: 'https://www.threads.net/@ykzts'
+  },
+  {
+    label: '山岸和利のXアカウント',
+    logo: (
+      <svg className={styles['icon-logo']}>
+        <use xlinkHref="#x-logo" />
+      </svg>
+    ),
+    url: 'https://twitter.com/ykzts'
+  }
+]
 
 export default function Contact() {
   return (
@@ -16,7 +71,7 @@ export default function Contact() {
 
       <SectionContent>
         <p>
-          山岸和利に対するお問い合わせや依頼はメールからお願いします。スケジュール次第ではありますが有期もしくは案件単位での作業依頼や技術相談でしたら有償で承ります。
+          山岸和利に対するお問い合わせやご依頼はメールからお願いします。スケジュール次第ではありますが有期もしくは案件単位での作業依頼や技術相談でしたら有償で承ります。
         </p>
         <p>
           ただし無償もしくは報酬が不明瞭な依頼に関してはお応えできかねます。また依頼主が不明であるスカウトメールやオファーメールにつきましてはご返答いたしかねますのであらかじめご容赦ください。
@@ -41,71 +96,19 @@ export default function Contact() {
             <h3 className={styles['list__title']}>Elsewhere</h3>
 
             <ul className={styles.icons}>
-              <li className={styles.icons__item}>
-                <Link
-                  aria-label="山岸和利のFacebookアカウント"
-                  className={styles['icon-link']}
-                  href="https://www.facebook.com/ykzts"
-                  rel="me"
-                  target="_blank"
-                >
-                  <svg className={styles['icon-logo']}>
-                    <use xlinkHref="#facebook-logo" />
-                  </svg>
-                </Link>
-              </li>
-              <li className={styles.icons__item}>
-                <Link
-                  aria-label="山岸和利のGitHubアカウント"
-                  className={styles['icon-link']}
-                  href="https://github.com/ykzts"
-                  rel="me"
-                  target="_blank"
-                >
-                  <svg className={styles['icon-logo']}>
-                    <use xlinkHref="#github-logo" />
-                  </svg>
-                </Link>
-              </li>
-              <li className={styles.icons__item}>
-                <Link
-                  aria-label="山岸和利のMastodonアカウント"
-                  className={styles['icon-link']}
-                  href="https://ykzts.technology/@ykzts"
-                  rel="me"
-                  target="_blank"
-                >
-                  <svg className={styles['icon-logo']}>
-                    <use xlinkHref="#mastodon-logo" />
-                  </svg>
-                </Link>
-              </li>
-              <li className={styles.icons__item}>
-                <Link
-                  aria-label="山岸和利のThreadsアカウント"
-                  className={styles['icon-link']}
-                  href="https://www.threads.net/@ykzts"
-                  rel="me"
-                  target="_blank"
-                >
-                  <svg className={styles['icon-logo']}>
-                    <use xlinkHref="#threads-logo" />
-                  </svg>
-                </Link>
-              </li>
-              <li className={styles.icons__item}>
-                <Link
-                  aria-label="山岸和利のXアカウント"
-                  className={styles['icon-link']}
-                  href="https://twitter.com/ykzts"
-                  rel="me"
-                  target="_blank"
-                >
-                  <svg className={styles['icon-logo']}>
-                    <use xlinkHref="#x-logo" />
-                  </svg>
-                </Link>
-              </li>
+              {socialLinks.map((socialLink) => (
+                <li className={styles.icons__item} key={socialLink.url}>
+                  <Link
+                    aria-label={socialLink.label}
+                    className={styles['icon-link']}
+                    href={socialLink.url}
+                    rel="me"
+                    target="_blank"
+                  >
+                    {socialLink.logo}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </li>
         </ul>
