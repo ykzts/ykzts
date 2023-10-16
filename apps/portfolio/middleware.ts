9ee1299e3e96e0fbeacaf0ea358ab06ef80a5df6
Yamagishi Kazutoshi <ykzts@desire.sh>
2023-10-16 10:12:15 +0900

    feat(portfolio): introduce csp (#1467)

---
+++ apps/portfolio/middleware.ts
@@ -0,0 +1,62 @@
+import { NextResponse } from 'next/server'
+
+function getNonce(): string {
+  const randomValues = crypto.getRandomValues(new Uint8Array(18))
+
+  return Buffer.from(randomValues).toString('base64')
+}
+
+function createCSPDirectives(): string[] {
+  const nonce = getNonce()
+
+  return [
+    "base-uri 'none'",
+    "connect-src 'self' https://vitals.vercel-insights.com",
+    "default-src 'none'",
+    "font-src 'self'",
+    "form-action 'none'",
+    "frame-ancestors 'none'",
+    "img-src 'self' data:",
+    `script-src 'nonce-${nonce}' 'strict-dynamic'`,
+    "style-src 'self' 'unsafe-inline'"
+  ]
+}
+
+export function middleware(request: Request) {
+  if (process.env.NODE_ENV !== 'production') {
+    return NextResponse.next()
+  }
+
+  const requestHeaders = new Headers(request.headers)
+  const cspDirectives = createCSPDirectives()
+
+  requestHeaders.set('Content-Security-Policy', cspDirectives.join('; '))
+
+  return NextResponse.next({
+    headers: new Headers({
+      'Content-Security-Policy': cspDirectives.join('; ')
+    }),
+    request: {
+      headers: requestHeaders
+    }
+  })
+}
+
+export const config = {
+  matcher: [
+    {
+      missing: [
+        {
+          key: 'Next-Router-Prefetch',
+          type: 'header'
+        },
+        {
+          key: 'Purpose',
+          type: 'header',
+          value: 'prefetch'
+        }
+      ],
+      source: '/((?!_next/(?:image|static)|api|favicon.ico).*)'
+    }
+  ]
+}
