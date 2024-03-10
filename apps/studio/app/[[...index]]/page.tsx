763035e96dc0170e801177daead98a22c75342df
Yamagishi Kazutoshi <ykzts@desire.sh>
2024-03-10 12:47:38 +0900

    fix(studio): use direct import

--- apps/studio/app/[[...index]]/page.tsx
+++ apps/studio/app/[[...index]]/page.tsx
@@ -1,9 +1,10 @@
-import { Studio } from './_components/studio'
+import { NextStudio } from 'next-sanity/studio'
+import sanityConfig from '@/sanity.config'
 
 export const dynamic = 'force-static'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
 export default function StudioPage() {
-  return <Studio />
+  return <NextStudio config={sanityConfig} {...props} />
 }
