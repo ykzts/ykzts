3060f4f2967dbc3179e67d9d9b23cef61652acfb
Yamagishi Kazutoshi <ykzts@desire.sh>
2024-10-16 13:42:41 +0900

    feat(web): disable loading screen (#2545)
    * feat(web): disable loading screen
    
    * fix(web): remove unused import
    
    * fix(web): always `opacity: 1`

--- apps/portfolio/app/_components/wrapper.tsx
+++ apps/portfolio/app/_components/wrapper.tsx
@@ -1,19 +1,10 @@
-'use client'
-
-import clsx from 'clsx'
 import { type ReactNode } from 'react'
-import { useInView } from 'react-intersection-observer'
 import styles from './wrapper.module.css'
 
-export default function LayoutWrapper({ children }: { children: ReactNode }) {
-  const { inView, ref } = useInView()
-
-  return (
-    <div
-      className={clsx(styles.wrapper, { [styles['wrapper--active']]: inView })}
-      ref={ref}
-    >
-      {children}
-    </div>
-  )
+export default function LayoutWrapper({
+  children
+}: Readonly<{
+  children: ReactNode
+}>) {
+  return <div className={styles.wrapper}>{children}</div>
 }
