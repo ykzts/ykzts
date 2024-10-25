774a397c36ac3c4069c94f0ce1731e7d8e8befc1
Yamagishi Kazutoshi <ykzts@desire.sh>
2024-10-25 01:18:19 +0000

    build(deps): replace tag for types-react

--- apps/portfolio/mdx-components.tsx
+++ apps/portfolio/mdx-components.tsx
@@ -1,16 +1,9 @@
-import { type MDXComponents } from 'mdx/types'
+import type { MDXComponents } from 'mdx/types'
 import Link from './components/link'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    a: ({
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      ref: _ref,
-      ...props
-    }) => (
-      // @ts-expect-error mdx/typesがReact v19に対応していないので無視させる。
-      <Link {...props} />
-    )
+    a: Link
   }
 }
