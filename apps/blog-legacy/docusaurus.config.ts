4b40aa19b3ebf6bc9a9413d76f0ab5cfce75a0e5
Yamagishi Kazutoshi <ykzts@desire.sh>
2024-12-08 02:41:46 +0900

    fix(blog-legacy): run `pnpm format`

--- apps/blog-legacy/docusaurus.config.ts
+++ apps/blog-legacy/docusaurus.config.ts
@@ -35,7 +35,8 @@ const config: Config = {
           blogDescription: description,
           blogSidebarCount: 0,
           blogSidebarTitle: '最近の投稿',
-          editUrl: 'https://github.com/ykzts/ykzts.blog/edit/main/apps/blog-legacy/',
+          editUrl:
+            'https://github.com/ykzts/ykzts.blog/edit/main/apps/blog-legacy/',
           feedOptions: {
             copyright,
             description,
