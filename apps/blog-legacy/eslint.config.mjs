0ff7f2bd87e5280e9af1ed6ea1e2222ccf561c8c
Yamagishi Kazutoshi <ykzts@desire.sh>
2024-12-08 18:06:31 +0900

    build(blog-legacy): introduce `@docusaurus/eslint-plugin` (#2739)

--- apps/blog-legacy/eslint.config.mjs
+++ apps/blog-legacy/eslint.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 
-import baseConfig from '@ykzts/eslint-config/next'
+import baseConfig from '@ykzts/eslint-config/docusaurus'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(...baseConfig, {
